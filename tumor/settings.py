31a990fc622e061300c0a58cb2b7337c3b98ccff
Charithaaa5712222 <247Y1A66J0@mlritm.ac.in>
2026-06-22 17:18:15 +0530

    Update settings.py

--- tumor/settings.py
+++ tumor/settings.py
@@ -74,14 +74,12 @@ WSGI_APPLICATION = 'Tumor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tumorDB',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
